d123a89b108be95604071a10f834421796cec32c
Joao Pedro Silveira <jpedross1999@gmail.com>
2021-08-22 19:09:45 -0300

    fix: change function visibility

--- action.py
+++ action.py
@@ -12,17 +12,17 @@ def play_game(initial_state: str, actions: list[str]) -> str:
 def perform_action(state: str, action: str) -> str:
     blank_position = state.index(constants.EMPTY_CHAR)
     if action == constants.UP:
-        return swap_elements(state, blank_position, blank_position - 3)
+        return __swap_elements(state, blank_position, blank_position - 3)
     elif action == constants.DOWN:
-        return swap_elements(state, blank_position, blank_position + 3)
+        return __swap_elements(state, blank_position, blank_position + 3)
     if action == constants.RIGHT:
-        return swap_elements(state, blank_position, blank_position + 1)
+        return __swap_elements(state, blank_position, blank_position + 1)
     elif action == constants.LEFT:
-        return swap_elements(state, blank_position, blank_position - 1)
+        return __swap_elements(state, blank_position, blank_position - 1)
     return state
 
 
-def swap_elements(string: str, fst_index: int, snd_index: int) -> str:
+def __swap_elements(string: str, fst_index: int, snd_index: int) -> str:
     """
     Troca dois caracteres de lugar
     :param string: palavra a ter dois caracteres trocados
